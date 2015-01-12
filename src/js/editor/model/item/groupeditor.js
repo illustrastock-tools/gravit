00e27cd5e5a755b96d201d7ba66eebbe7895b735
Anna Ignatenkova <ann.ign@mail.ru>
2015-01-12 03:20:12 +0300

    Paint outline of group/layer members instead of bbox during transformation

--- src/js/editor/model/item/groupeditor.js
+++ src/js/editor/model/item/groupeditor.js
@@ -15,17 +15,39 @@
 
     /** override */
     GGroupEditor.prototype._paintOutline = function (transform, context, paintBBox, color) {
-        if (!this._transform) {
+        if (!this._transform || !this._editors) {
             GBlockEditor.prototype._paintOutline.call(this, transform, context, paintBBox, color);
         } else {
             // We are in transformation, show children outline instead of self bbox
-            var element = this.getPaintElement();
-            for (var node = element.getFirstChild(); node != null; node = node.getNext()) {
-                if (node instanceof GElement) {
-                    GElementEditor.openEditor(node)._paintOutline(transform, context, paintBBox, color);
-                }
+            for (var i = 0; i < this._editors.length; ++i) {
+                var ed = this._editors[i];
+                ed._paintOutline(transform, context, paintBBox, color);
             }
         }
+    };
+
+    /** @override */
+    GGroupEditor.prototype._setTransform = function (transform) {
+        var element = this.getElement();
+        for (var node = element.getFirstChild(); node != null; node = node.getNext()) {
+            if (node instanceof GElement) {
+                var ed = GElementEditor.openEditor(node);
+                ed._setTransform(transform);
+            }
+        }
+
+        GBlockEditor.prototype._setTransform.call(this, transform);
+    };
+
+    /** @override */
+    GGroupEditor.prototype.resetTransform = function () {
+        for (var i = this._editors ? this._editors.length : 0; i > 0; --i) {
+            var ed = this._editors[i-1];
+            ed.resetTransform();
+            GElementEditor.closeEditor(ed.getElement());
+        }
+
+        GBlockEditor.prototype.resetTransform.call(this);
     };
 
     /** @override */
